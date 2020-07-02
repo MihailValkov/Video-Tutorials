6833260280c31b94bb9b1fcab43e310f2bcbf031
Mihail <mihail_valkov@mail.bg>
2020-07-02 15:47:37 +0300

    feat/Logout implement logout logic

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@
 module.exports = {
     get : {
         home(req,res,next){
-            res.render('home/home.hbs',req.locals)
+            res.render('home/home.hbs',{title:"Home Page", ...req.locals})
         }
     },
     post : {}
